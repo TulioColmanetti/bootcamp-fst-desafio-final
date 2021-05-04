d2f78dadbadbdcef80c1dd164d6ef900cf7724f4
Tulio Colmanetti <tulio.colmanetti@gmail.com>
2021-05-04 17:54:23 -0300

    Import created React components, implemented list transactions from selected period using API

--- client/src/pages/PeriodPage.js
+++ client/src/pages/PeriodPage.js
@@ -1,11 +1,41 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Header from '../components/Header.js';
+import Main from '../components/Main.js';
+import Select from '../components/Select.js';
+import Summary from '../components/Summary.js';
+import Filter from '../components/Filter.js';
+import Transactions from '../components/Transactions.js';
+import api from '../api/apiTransaction.js';
 
 export default function PeriodPage() {
+  const [selectedPeriod, setSelectedPeriod] = useState('2021-01');
+  const [transactions, setTransactions] = useState([]);
+
+  useEffect(() => {
+    const getAllTransactions = async () => {
+      const allTransactions = await api.getAll('?yearMonth=' + selectedPeriod);
+      setTransactions(allTransactions.data);
+    };
+    getAllTransactions();
+  }, [selectedPeriod]);
+
+  const handleSelectedPeriod = (period) => {
+    setSelectedPeriod(period);
+  };
+
   return (
-    <div>
+    <div className="container">
       <Header>Bootcamp Full Stack - Desafio Final</Header>
-      <h3>Controle Financeiro Pessoal</h3>
+      <Main>
+        <Select
+          options={['2021-01', '2021-02', '2021-03']}
+          selectedValue={selectedPeriod}
+          onSelect={handleSelectedPeriod}
+        />
+        <Summary />
+        <Filter />
+        <Transactions>{JSON.stringify(transactions)}</Transactions>
+      </Main>
     </div>
   );
 }
