1d4c297023a8382ad368886780e4205d35a43011
Tulio Colmanetti <tulio.colmanetti@gmail.com>
2021-05-04 21:16:41 -0300

    Removed excessive CSS style

--- client/src/components/Main.js
+++ client/src/components/Main.js
@@ -1,24 +1,12 @@
 import React from 'react';
 
 export default function Main({ children }) {
-  const { containerStyle } = styles;
-
   return (
-    <main className="container">
-      <div style={containerStyle}>
+    <main>
+      <div className="center">
         <h5>Controle Financeiro Pessoal</h5>
         <div>{children}</div>
       </div>
     </main>
   );
 }
-
-const styles = {
-  containerStyle: {
-    display: 'flex',
-    flexDirection: 'column',
-    alignItems: 'center',
-    justifyContent: 'center',
-    margin: '20px',
-  },
-};
