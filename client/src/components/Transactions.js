0d76b26527ed15851b615c076dd25a07d6642ab9
Tulio Colmanetti <tulio.colmanetti@gmail.com>
2021-05-04 21:13:24 -0300

    Changed Transactions component to use Transaction

--- client/src/components/Transactions.js
+++ client/src/components/Transactions.js
@@ -1,5 +1,12 @@
 import React from 'react';
+import Transaction from './Transaction';
 
-export default function Transactions({ children }) {
-  return <div>{children}</div>;
+export default function Transactions({ children: transactions }) {
+  return (
+    <div>
+      {transactions.map((transaction) => {
+        return <Transaction key={transaction._id}>{transaction}</Transaction>;
+      })}
+    </div>
+  );
 }
