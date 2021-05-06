cd6d35aaf4cc4ca408b9d11187277d1892f4b3e1
Tulio Colmanetti <tulio.colmanetti@gmail.com>
2021-05-06 20:02:33 -0300

    Added functionality to handle Modal window and persist its data

--- client/src/pages/PeriodPage.js
+++ client/src/pages/PeriodPage.js
@@ -5,6 +5,7 @@ import Select from '../components/Select.js';
 import Summary from '../components/Summary.js';
 import Filter from '../components/Filter.js';
 import Transactions from '../components/Transactions.js';
+import ModalTransaction from '../components/ModalTransaction.js';
 import api from '../api/apiTransaction.js';
 import * as stringHelpers from '../helpers/stringHelpers.js';
 
@@ -25,11 +26,12 @@ function generateCurrentPeriod() {
 export default function PeriodPage() {
   const [selectedPeriod, setSelectedPeriod] = useState(generateCurrentPeriod());
   const [transactions, setTransactions] = useState([]);
+  const [isModalOpen, setIsModalOpen] = useState(false);
 
   useEffect(() => {
     const getAllTransactions = async () => {
-      const allTransactions = await api.getAll('?yearMonth=' + selectedPeriod);
-      setTransactions(allTransactions.data);
+      const allTransactions = await api.getTransactionsFrom(selectedPeriod);
+      setTransactions(allTransactions);
     };
     getAllTransactions();
   }, [selectedPeriod]);
@@ -38,17 +40,55 @@ export default function PeriodPage() {
     setSelectedPeriod(period);
   };
 
+  const handlePersistData = async (newTransaction) => {
+    const createdTransaction = await api.postTransaction(newTransaction);
+
+    const newTransactions = Object.assign([], transactions);
+    newTransactions.push(createdTransaction);
+    newTransactions.sort((a, b) => a.day - b.day);
+
+    setTransactions(newTransactions);
+
+    setIsModalOpen(false);
+  };
+
+  const handleModalClose = () => {
+    setIsModalOpen(false);
+  };
+
+  const handleButtonClick = () => {
+    setIsModalOpen(true);
+  };
+
   return (
     <div className="container">
       <Header>Bootcamp Full Stack - Desafio Final</Header>
       <Main>
-        <Select
-          options={api.ALL_PERIODS}
-          selectedValue={selectedPeriod}
-          onSelect={handleSelectedPeriod}
-        />
+        {!isModalOpen && (
+          <Select
+            options={api.ALL_PERIODS}
+            selectedValue={selectedPeriod}
+            onSelect={handleSelectedPeriod}
+          />
+        )}
         <Summary>{transactions}</Summary>
+        {!isModalOpen && (
+          <button
+            className="waves-effect waves-green btn"
+            onClick={handleButtonClick}
+          >
+            + NOVO LANÇAMENTO
+          </button>
+        )}
         <Filter />
+        {isModalOpen && (
+          <ModalTransaction
+            onSave={handlePersistData}
+            onClose={handleModalClose}
+          >
+            {}
+          </ModalTransaction>
+        )}
         <Transactions>{transactions}</Transactions>
       </Main>
     </div>
