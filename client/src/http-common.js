55bfe6bbd99b6c1eb28f0564c889ddd7a0fdc7f4
Tulio Colmanetti <tulio.colmanetti@gmail.com>
2021-05-07 20:22:37 -0300

    Removed hard-coded url pointing to localhost

--- client/src/http-common.js
+++ client/src/http-common.js
@@ -2,7 +2,8 @@ import axios from 'axios';
 
 //Define base URL of API services
 export default axios.create({
-  baseURL: 'http://localhost:3001/',
+  // baseURL: 'http://localhost:3001/',
+  baseURL: '',
   headers: {
     'Content-type': 'application/json',
   },
